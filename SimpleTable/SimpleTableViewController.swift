44a52235a99cb76b5f89626e5f272839fe4ce9fe
chiyoungwu <chiyoungwu@tencent.com>
2015-06-07 11:34:28 +0800

    Chapter 1: Simple TableView

---
+++ SimpleTable/SimpleTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 夜游 on 15/6/7.
+//  Copyright (c) 2015年 夜游. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
