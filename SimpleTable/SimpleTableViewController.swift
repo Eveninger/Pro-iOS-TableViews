e04381d7907ea54d001fdeba71791cfe05aa5e2a
MonkeyDNighter <2241813603@qq.com>
2015-06-07 20:45:15 +0800

    完成第一章示例，一个简单的TableView应用

--- SimpleTable/SimpleTableViewController.swift
+++ SimpleTable/SimpleTableViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class SimpleTableViewController: UIViewController {
+class SimpleTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var tableData: [String]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableData = [String]()
+        for i in 0...10 {
+            var data = NSString(format: "Item %d", i) as String
+            tableData.append(data)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIndentifier = "Cell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIndentifier) as? UITableViewCell
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIndentifier)
+        }
+        
+        cell!.textLabel!.text = self.tableData[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Table row \(indexPath.row) has been tapped")
+        var message = "You Tapped row \(indexPath.row)"
+        var alert = UIAlertView(title: "Row Tapped", message: message, delegate: self, cancelButtonTitle: "OK")   
+        alert.show()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
